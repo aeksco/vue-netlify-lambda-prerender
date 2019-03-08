04458d7528503cb0d1859d9d113360f38fd345d5
Alexander Schwartzberg <aeksco@gmail.com>
2019-03-07 20:11:45 -0500

    Removed setTimeout in fetchMessage Vuex action

--- src/store.js
+++ src/store.js
@@ -18,12 +18,8 @@ export default new Vuex.Store({
       commit('loading', true)
       axios.get('/.netlify/functions/hello')
       .then(({ data }) => {
-        // Adds a 1 second delay to illustrate asyc behavior
-        // setTimeout can/should be removed
-        setTimeout(() => {
-          commit('msg', data.msg)
-          commit('loading', false)
-        }, 1000)
+        commit('msg', data.msg)
+        commit('loading', false)
       })
       .catch(() => {
         commit('loading', false)
